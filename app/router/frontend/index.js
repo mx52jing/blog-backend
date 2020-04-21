1429872ea28bcaebdd5b37a029da7a38954b6023
mengxuan <3007492783@qq.com>
2020-04-21 18:18:54 +0800

    add category interface

--- app/router/frontend/index.js
+++ app/router/frontend/index.js
@@ -7,4 +7,6 @@ module.exports = app => {
 	const { router, controller } = app;
 	/* 文章 */
 	router.resources('articles', '/frontend/articles', controller.frontend.articles);
+    /* 分类 */
+    router.resources('categories', '/frontend/categories', controller.frontend.categories);
 };
