b99e6ef2e14631aa74145d7a3bb09b829e79aaa9
mx52jing <mx52jing@gmail.com>
2020-05-10 23:49:20 +0800

    修改分类controller

--- app/controller/frontend/categories.js
+++ app/controller/frontend/categories.js
@@ -7,9 +7,23 @@ class CategoryController extends BasicController {
 	async index() {
 		const { ctx } = this
 		try {
-			const { Category } = ctx.model,
-				categories = await Category.find({})
-			this.handleSuccess(categories)
+			const { Category, Article } = ctx.model,
+				categories = await Category.find({}),
+				{ name } = ctx.request.query
+			if(!!name) {
+				const data = await Article.find({ category: { $in: [name] }})
+				this.handleSuccess(data)
+				return
+			}
+			const newCategoryList = []
+			for (let category of categories) {
+				const articleCount = await Article.countDocuments({ category: { $in: [category.name] }})
+				newCategoryList.push({
+					...category.toObject(),
+					articleCount
+				})
+			}
+			this.handleSuccess(newCategoryList)
 		} catch (e) {
 			this.handleError(e);
 		}
